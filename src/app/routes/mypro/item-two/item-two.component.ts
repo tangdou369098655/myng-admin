1f0c00211175658fec84cb17ef7dd2dd06ee6ad1
tangdou369098655 <369098655@qq.com>
2019-08-22 23:32:16 +0800

    文件上传与读取

--- src/app/routes/mypro/item-two/item-two.component.ts
+++ src/app/routes/mypro/item-two/item-two.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { _HttpClient } from '@delon/theme';
 @Component({
   selector: 'app-item-two',
   templateUrl: './item-two.component.html',
   styleUrls: ['./item-two.component.less']
 })
 export class ItemTwoComponent implements OnInit {
-
-  constructor() { }
+  private status = [];
+  private mystatus;
+  constructor(private http:_HttpClient) { }
 
   ngOnInit() {
+    let url='http://localhost:4200/assets/myjson/mydata.json';
+    this.http.get(url).subscribe((res:any)=>{
+      this.status=res.data1
+      console.log(this.status)
+    })
   }
-
+  myUpload(e){
+    console.log(e)
+    let fileReader = new FileReader();
+    fileReader.onload=()=>{
+      console.log(fileReader.result)
+    }
+    // fileReader.readAsText(e.target.files[0], 'utf-8')
+    fileReader.readAsText(e.target.files[0])
+  }
 }
+
--- src/app/routes/mypro/wlblock/wlblock.component.ts
+++ src/app/routes/mypro/wlblock/wlblock.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit, ViewChild, TemplateRef, ChangeDetectionStrategy, Cha
 import { NzMessageService, NzModalService } from 'ng-zorro-antd';
 import { _HttpClient } from '@delon/theme';
 import { tap, map } from 'rxjs/operators';
+import Clipboard from '../../../../clipboard.min.js';
 import { STComponent, STColumn, STData, STChange } from '@delon/abc';
+
 
 @Component({
   selector: 'app-wlblock',
@@ -12,16 +14,34 @@ import { STComponent, STColumn, STData, STChange } from '@delon/abc';
 export class WlblockComponent implements OnInit {
   private status = [];
   private mystatus;
-  constructor(private http:_HttpClient) { }
+  private myData2;
+  private myData;
+  constructor(private http:_HttpClient ) { }
 
   ngOnInit() {//初始化时候，异步请求服务器端的首页数据
     let url='http://localhost:4200/assets/myjson/mydata.json';
     this.http.get(url).subscribe((res:any)=>{
       this.status=res.data1
+      console.log(this.status)
     })
   }
   myclick(e:MouseEvent){
     e.preventDefault();
     console.log(e)
+  }
+  getData1(e){
+    var clipboard = new Clipboard('btn');
+ 
+clipboard.on('success', function(e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+    e.clearSelection();
+});
+ 
+clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+});
   }
 }
