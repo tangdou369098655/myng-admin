a06c4be471c4d2102de0e7ba73d11a64e62d5c3d
tangdou369098655 <369098655@qq.com>
2019-10-06 21:20:30 +0800

    10-6

--- src/app/layout/default/sidebar/sidebar.component.ts
+++ src/app/layout/default/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { SettingsService } from '@delon/theme';
 
 @Component({
@@ -6,6 +6,16 @@ import { SettingsService } from '@delon/theme';
   templateUrl: './sidebar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SidebarComponent {
-  constructor(public settings: SettingsService) {}
+export class SidebarComponent implements OnInit {
+  constructor(
+    public settings: SettingsService
+    ) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      const sidebarNav = document.querySelector('.sidebar-nav')
+      const links: HTMLLinkElement[] = Array.from(sidebarNav.querySelectorAll('.sidebar-nav__item-link'))
+      links.forEach(_ => _.title = _.textContent)
+    }, 2000)
+  }
 }
--- src/app/routes/mypro/item-two/item-two.component.ts
+++ src/app/routes/mypro/item-two/item-two.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { _HttpClient } from '@delon/theme';
+import { ViewChild, OnDestroy, AfterViewInit } from '@angular/core';
+import { STColumn, STPage, STComponent } from '@delon/abc';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 @Component({
   selector: 'app-item-two',
   templateUrl: './item-two.component.html',
   styleUrls: ['./item-two.component.less']
 })
-export class ItemTwoComponent implements OnInit {
+export class ItemTwoComponent implements OnInit ,AfterViewInit, OnDestroy{
   private status = [];
   private mystatus;
   constructor(private http:_HttpClient) { }
@@ -26,5 +30,51 @@ export class ItemTwoComponent implements OnInit {
     // fileReader.readAsText(e.target.files[0], 'utf-8')
     fileReader.readAsText(e.target.files[0])
   }
+  private destroy$ = new Subject();
+  @ViewChild('st', { static: false }) st: STComponent;
+
+  page: STPage = {
+    front: false,
+    show: false,
+  };
+  data: any[] = Array(2000)
+    .fill({})
+    .map((_item: any, idx: number) => {
+      return {
+        id: idx + 1,
+        price: ~~(Math.random() * 100),
+        price2:'sffsfsdfsdfsdfsdfsdfsdfsd'
+      };
+    });
+  columns: STColumn[] = [
+    { title: '编号', index: 'id', width: 100 },
+    { title: '价格1', index: 'price2', width: 100 },
+    { title: '价格2', index: 'price', width: 100 },
+    { title: '价格3', index: 'price', width: 100 },
+    { title: '价格4', index: 'price', width: 100 },
+    { title: '价格5', index: 'price', width: 100 },
+    { title: '价格6', index: 'price', width: 100 },
+    { title: '价格7', index: 'price', width: 100 },
+    { title: '价格8', index: 'price', width: 100 },
+    { title: '价格9', index: 'price', width: 100 },
+    { title: '价格10', index: 'price', width: 100 },
+  ];
+
+  scrollToIndex(index: number): void {
+    this.st.cdkVirtualScrollViewport.scrollToIndex(index);
+  }
+
+  ngAfterViewInit(): void {
+    this.st.cdkVirtualScrollViewport.scrolledIndexChange.pipe(takeUntil(this.destroy$)).subscribe(data => {
+      console.log('scroll index to', data);
+      
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
+
 
